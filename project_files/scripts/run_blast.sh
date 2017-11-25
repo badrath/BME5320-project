fb105d24463ea25c1c789519e3e9652c53634c19
rmar1478 <rmarini1478@gmail.com>
2017-11-24 18:21:44 -0600

    instead of num_descriptions, trying -max_num_seqs

--- project_files/scripts/run_blast.sh
+++ project_files/scripts/run_blast.sh
@@ -15,4 +15,7 @@ NUM_ALIGNMENTS_DESCR=500
 
 TAB_FORMAT=6
 
-$BLASTP -query $QUERY_FILE -out $OUTPUT_FILE -db $DB -num_alignments $NUM_ALIGNMENTS_DESCR -num_descriptions $NUM_ALIGNMENTS_DESCR -outfmt $TAB_FORMAT
+#$BLASTP -query $QUERY_FILE -out $OUTPUT_FILE -db $DB -num_alignments $NUM_ALIGNMENTS_DESCR -num_descriptions $NUM_ALIGNMENTS_DESCR -outfmt $TAB_FORMAT
+
+
+$BLASTP -query $QUERY_FILE -out $OUTPUT_FILE -db $DB -num_alignments $NUM_ALIGNMENTS_DESCR -max_target_seqs $NUM_ALIGNMENTS_DESCR -outfmt $TAB_FORMAT
